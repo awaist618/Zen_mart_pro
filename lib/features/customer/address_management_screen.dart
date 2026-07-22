e040755831dafe4864585ec2deade7b46d3bccf1
Awais Tariq <at2544344@gmail.com>
2026-07-22 23:30:21 +0500

    feat: comprehensive UI polish, navigation safety, and bug fixes

--- lib/features/customer/address_management_screen.dart
+++ lib/features/customer/address_management_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../core/providers.dart';
 import '../../models/address_model.dart';
